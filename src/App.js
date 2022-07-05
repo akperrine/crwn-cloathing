5aead7a2b2d40d1cf45d17394d491bd4fedb26c3
Austin Perrine <akperrine@gmail.com>
2022-07-04 21:04:43 -0600

    Navigation Routes with Shop and Sign In links (components still not built)

--- src/App.js
+++ src/App.js
@@ -1,43 +1,23 @@
 import "./categories.styles.scss";
-// import CategoryItem from "./components/category-item/Category-Item";
-import Directory from "./components/Directory/Directory";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Signin from "./routes/sign-in/signin.component";
+
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
+  const Shop = () => {
+    return <h1>This is the shop page</h1>;
+  };
 
   return (
-    <Directory categories={categories} />
-    // <div className="categories-container">
-    //   {categories.map((category) => (
-    //     <CategoryItem key={category.id} category={category} />
-    //   ))}
-    // </div>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/sign-in" element={<Signin />} />
+      </Route>
+    </Routes>
   );
 };
 
