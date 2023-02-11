74034bce6c0f513c79fb8784dcbf3f6c23ac38ab
Austin Perrine <akperrine@gmail.com>
2023-02-11 12:37:34 -0700

    use reselect to memoize category selector

--- src/routes/category/category.jsx
+++ src/routes/category/category.jsx
@@ -7,11 +7,11 @@ import ProductCard from "../../components/product-card/product-card";
 import { CategoryContainer, Title } from "./category.styles";
 
 //
-import { selectCategories } from "../../store/categories/category.selector";
+import { selectCategoriesMap } from "../../store/categories/category.selector";
 
 const Category = () => {
   const { category } = useParams();
-  const categoriesMap = useSelector(selectCategories);
+  const categoriesMap = useSelector(selectCategoriesMap);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
